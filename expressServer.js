1eefe513acbcc6fe9f64112109091db759e40d7b
Amanda Allen <amanda.allen2488@gmail.com>
2017-06-01 15:15:00 -0500

    added bonus solutions

--- expressServer.js
+++ expressServer.js
@@ -2,27 +2,46 @@
 
 var fs = require('fs');
 var path = require('path');
-var petsPath = path.join(__dirname, 'pets.json');
-
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8000;
 
+var petsPath = path.join(__dirname, 'pets.json');
+var port = process.env.PORT || 8000;
 var data = fs.readFileSync(petsPath,'utf-8');
 var petData = JSON.parse(data);
 
 app.disable('x-powered-by');
 
-app.get('/pets', function(req, res) {
-    res.send(petData);
+app.route('/pets')
+.get(function (req, res) {
+  res.send(petData);
+})
+.post(function (req, res) {
+  console.log(req.body);
+  var body = '';
+  req.on('data', function(data) {
+    body += data;
   });
+  req.on('end', function() {
+    body = JSON.parse(body);
+    if (body.age && body.name && body.kind){
+      var pets = petData;
+      pets.push(body);
+      var petsJSON = JSON.stringify(pets);
+      fs.writeFileSync(petsPath,petsJSON);
+      res.set('Content-type', 'application/json');
+      res.send(JSON.stringify(body));
+    } else {
+      res.sendStatus(400);
+    }
+  });
+})
 
 app.get('/pets/:id', function(req,res){
   var id = req.params.id;
-  if (id < 0 || id >= petData.length || Number.isNaN(id)) {
+  if (id < 0 || id >= petData.length || isNaN(id)) {
     return res.sendStatus(404);
   };
-
   res.set('Content-type', 'application/json');
   res.send(petData[id]);
 })
@@ -34,3 +53,5 @@ app.use(function(req, res){
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
+
+module.exports = app;
